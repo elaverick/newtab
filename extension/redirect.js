f27c9c5a949653e4f14cf5674236ea10e071aadc
elaverick <elaverick@hotmail.com>
2026-09-19 18:00:57 +0100

    Add configurable new tab redirect

---
+++ extension/redirect.js
@@ -0,0 +1,13 @@
+/*
+ * Set this to the GitHub Pages URL for your fork.
+ *
+ * Example:
+ * https://your-user.github.io/your-repository/
+ */
+
+const LIMINAL_NEW_TAB_URL =
+    "https://elaverick.github.io/newtab/";
+
+window.location.replace(
+    LIMINAL_NEW_TAB_URL
+);
